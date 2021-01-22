050b48c77221a3f5c6bff3da2699f914c4430e33
gschare <grschare1315@gmail.com>
2021-01-22 02:24:22 -0800

    Day 19 (both parts)

--- day19/part2.hs
+++ day19/part2.hs
@@ -6,12 +6,19 @@ import Data.Char (isDigit, isAlpha)
 import Control.Applicative
 import Control.Arrow
 
-data Rule = Rule `Seq` Rule | Rule `Alt` Rule | Val Char | Id Int deriving (Show)
+data Rule = Rule `Seq` Rule | Rule `Alt` Rule | Val Char | Id Int deriving (Show, Eq)
+
+rule8 :: Rule
+rule8 = (Id 42) `Alt` ((Id 42) `Seq` (Id 8))
+
+rule11 :: Rule
+rule11 = ((Id 42) `Seq` (Id 31)) `Alt` (((Id 42) `Seq` (Id 11)) `Seq` (Id 31))
 
 main :: IO ()
 main = do
     (rules, msgs) <- parseFile "input.txt"
-    print $ length $ filter (match (Id 0) rules) msgs
+    let rules' = [(8, rule8), (11, rule11)] ++ filter (\(i,_) -> i /= 8 && i /= 11) rules
+    print $ length $ filter (match (Id 0) rules') msgs
 
 int :: ReadP Int
 int = read <$> many1 (satisfy isDigit)
@@ -41,7 +48,7 @@ message (r1 `Seq` r2) rules = (++) <$> message r1 rules <*> message r2 rules
 message (s1 `Alt` s2) rules = message s1 rules <|> message s2 rules
 
 match :: Rule -> [(Int, Rule)] -> String -> Bool
-match r rules msg = case readP_to_S (message r rules) msg of
+match r rules msg = case readP_to_S (message r rules <* eof) msg of
                       [] -> False
                       [(_,"")] -> True
                       _ -> False
